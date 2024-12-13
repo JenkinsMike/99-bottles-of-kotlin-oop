1b95087c7ca45458dc7265737e42f294597005b9
Mike Jenkins <jenkins.mike@gmail.com>
2024-12-13 12:18:42 -0500

    Get by bottles sync'd.
    Getting previous (read, two-year-old) 99 Bottles code in my personal repo.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val bottles = Bottles()
+    println(bottles.song())
+
+    val aSixPack = Bottles(6)
+    println(aSixPack.song())
+
+//    val lotsOfBottles = Bottles(999, 0)
+//    println(lotsOfBottles.song())
+}
